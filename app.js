2f8c5e57c2093be8d25ac2f01a2507d067a5461d
Alistair Willis <alistairrwillis@gmail.com>
2017-01-31 11:58:04 +0900

    add handlebars view engine

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 var express = require('express'); 
+
+var app = express(); 
+
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', '.hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render(__dirname + '/public/index.hbs');
+});
+
+app.get('/search', function(req, res) {
+    var title = req.query.title; 
+    console.log(title); 
+
+    res.render(__dirname + '/public/index.hbs', {
+        title: title
+    }); 
+});
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000'); 
+})
